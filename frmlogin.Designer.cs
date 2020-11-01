2b23af702deb613221b35b0eb3561d35a0d254e9
dungpk14789 <62883445+dungpk14789@users.noreply.github.com>
2020-11-01 15:36:45 +0700

    cfinish frmlogin

--- frmlogin.Designer.cs
+++ frmlogin.Designer.cs
@@ -29,29 +29,49 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmlogin));
-            this.pblogin = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.pbExit = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pblogin)).BeginInit();
-            this.panel1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pblogin = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbExit)).BeginInit();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogin)).BeginInit();
             this.SuspendLayout();
             // 
-            // pblogin
+            // pbExit
             // 
-            this.pblogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pblogin.BackgroundImage")));
-            this.pblogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pblogin.Location = new System.Drawing.Point(105, 116);
-            this.pblogin.Name = "pblogin";
-            this.pblogin.Size = new System.Drawing.Size(623, 545);
-            this.pblogin.TabIndex = 0;
-            this.pblogin.TabStop = false;
-            this.pblogin.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pbExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbExit.BackgroundImage")));
+            this.pbExit.Location = new System.Drawing.Point(3, 12);
+            this.pbExit.Name = "pbExit";
+            this.pbExit.Size = new System.Drawing.Size(56, 56);
+            this.pbExit.TabIndex = 3;
+            this.pbExit.TabStop = false;
+            this.pbExit.Click += new System.EventHandler(this.pbExit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pbExit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(1107, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(93, 720);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(789, 393);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(411, 33);
+            this.textBox1.TabIndex = 5;
             // 
             // label1
             // 
@@ -63,24 +83,14 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Let\'s Sign in";
             // 
-            // panel1
+            // txtAccount
             // 
-            this.panel1.Controls.Add(this.pbExit);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(1107, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(93, 720);
-            this.panel1.TabIndex = 2;
-            // 
-            // pbExit
-            // 
-            this.pbExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbExit.BackgroundImage")));
-            this.pbExit.Location = new System.Drawing.Point(3, 12);
-            this.pbExit.Name = "pbExit";
-            this.pbExit.Size = new System.Drawing.Size(56, 56);
-            this.pbExit.TabIndex = 3;
-            this.pbExit.TabStop = false;
-            this.pbExit.Click += new System.EventHandler(this.pbExit_Click);
+            this.txtAccount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAccount.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAccount.Location = new System.Drawing.Point(789, 309);
+            this.txtAccount.Name = "txtAccount";
+            this.txtAccount.Size = new System.Drawing.Size(411, 33);
+            this.txtAccount.TabIndex = 4;
             // 
             // pictureBox1
             // 
@@ -93,29 +103,55 @@
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
-            // txtAccount
+            // pblogin
             // 
-            this.txtAccount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtAccount.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAccount.Location = new System.Drawing.Point(789, 264);
-            this.txtAccount.Name = "txtAccount";
-            this.txtAccount.Size = new System.Drawing.Size(411, 33);
-            this.txtAccount.TabIndex = 4;
+            this.pblogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pblogin.BackgroundImage")));
+            this.pblogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pblogin.Location = new System.Drawing.Point(105, 116);
+            this.pblogin.Name = "pblogin";
+            this.pblogin.Size = new System.Drawing.Size(623, 545);
+            this.pblogin.TabIndex = 0;
+            this.pblogin.TabStop = false;
+            this.pblogin.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(789, 329);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(411, 33);
-            this.textBox1.TabIndex = 5;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(785, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(785, 359);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(176)))), ((int)(((byte)(211)))));
+            this.btnLogin.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(896, 461);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(200, 50);
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "Log In";
+            this.btnLogin.UseVisualStyleBackColor = false;
             // 
             // frmlogin
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1200, 720);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtAccount);
             this.Controls.Add(this.pictureBox1);
@@ -127,10 +163,10 @@
             this.Name = "frmlogin";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmlogin_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pblogin)).EndInit();
-            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbExit)).EndInit();
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogin)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,13 +174,16 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pblogin;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pbExit;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox txtAccount;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAccount;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pblogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
