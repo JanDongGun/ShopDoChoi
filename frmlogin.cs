5361aac5214a1a324df6254f30148b7d0b74dd39
dungpk14789 <62883445+dungpk14789@users.noreply.github.com>
2020-11-01 13:04:22 +0700

    create frmlogin

--- frmlogin.cs
+++ frmlogin.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuanLyShopDoChoi
 {
-    public partial class Form1 : Form
+    public partial class frmlogin : Form
     {
-        public Form1()
+        public frmlogin()
         {
             InitializeComponent();
         }
