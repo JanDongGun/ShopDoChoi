030dd89b4e5a143a4a55726382d45d88def332f9
dungpk14789 <62883445+dungpk14789@users.noreply.github.com>
2020-11-01 13:46:33 +0700

    add picture

--- frmlogin.Designer.cs
+++ frmlogin.Designer.cs
@@ -28,36 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmlogin));
+            this.pblogin = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogin)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pblogin
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(247, 150);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(451, 315);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pblogin.Image = ((System.Drawing.Image)(resources.GetObject("pblogin.Image")));
+            this.pblogin.Location = new System.Drawing.Point(56, 80);
+            this.pblogin.Name = "pblogin";
+            this.pblogin.Size = new System.Drawing.Size(670, 538);
+            this.pblogin.TabIndex = 0;
+            this.pblogin.TabStop = false;
+            this.pblogin.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(874, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Let\'s Sign in";
             // 
             // frmlogin
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1200, 720);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pblogin);
             this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmlogin";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pblogin)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pblogin;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- frmlogin.cs
+++ frmlogin.cs
@@ -16,5 +16,10 @@ namespace QuanLyShopDoChoi
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pblogin.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
     }
 }
